d845ed46a5c336e687c67385d8eeded7fac7d76d
roczhang <1587839905@qq.com>
2021-10-09 12:38:32 +0800

    lsa

--- LSA/Demo2.py
+++ LSA/Demo2.py
@@ -1,5 +1,8 @@
 import os
 import re
+from gensim import corpora, models
+from gensim.similarities import Similarity
+
 dir = '/Users/roczhang/Downloads/2016/'
 
 # 1.读取文件夹里面的文件
@@ -7,77 +10,70 @@ files = os.listdir(dir)
 strs = []
 for file in files:
     with open(dir+file, 'r') as file_to_read:
+        temp = ''
         while True:
             lines = file_to_read.readline()  # 整行读取
             if not lines:  # 如果读到txt文件结尾了，读下一个文件
                 break
-            lines = lines.strip('\n')  # 去掉文件结尾的换行符号
+            # lines = lines.strip('\n')  # 去掉文件结尾的换行符号
+            lines = re.sub('[^\u4e00-\u9fa5]+', '', lines)  # 只保留汉字
             if len(lines) == 0:  # 如果读到空行，不记录
                 continue
-            strs.append(lines)
-print(strs)
+            temp = temp + lines
+        strs.append(temp)
+print(len(strs))
 
-
-# 2.使用jieba分词器分词
 
 import jieba
 
-strs_seg = []
+strs_word = []
+# 分词
 for str in strs:
-    seg_list = jieba.cut(str, use_paddle=True)
-    seg_list = list(seg_list)
-    print(seg_list)
-    strs_seg.append(seg_list)
+    temp = jieba.cut(str, use_paddle=True)
+    strs_word.append(list(temp))
+print(strs_word)
 
-print('********************* strs_seg *******************************')
-print(strs_seg)      # 这里将所有的文本分词了，没有去重复，没有去标点
+# 生成字段和向量语料
+dictionary = corpora.Dictionary(strs_word)
+# print("dictionary: " + str(dictionary))
 
-strs_seg = [i for item in strs_seg for i in item]     # 将所有文本放在一个列表里面
+corpus = [dictionary.doc2bow(text) for text in strs_word]
+print(corpus)
 
-print('********************* set_strs *******************************')
+# 生成模型
+model = models.LsiModel(corpus, id2word=dictionary, num_topics=3)
 
-set_strs = set(strs_seg)  # 去重
-print(set_strs)
+vectorized_corpus = model[corpus]
 
-strs_list = list(set_strs)  # 转换成列表
-
+print(list(vectorized_corpus))
 
 
-# 这个函数是用来删除标点符号，数字和日期的。
-def regex_change(line):
-    #前缀的正则
-    username_regex = re.compile(r"^\d+::")
-    #URL，为了防止对中文的过滤，所以使用[a-zA-Z0-9]而不是\w
-    url_regex = re.compile(r"""
-        (https?://)?
-        ([a-zA-Z0-9]+)
-        (\.[a-zA-Z0-9]+)
-        (\.[a-zA-Z0-9]+)*
-        (/[a-zA-Z0-9]+)*
-    """, re.VERBOSE|re.IGNORECASE)
-    #剔除日期
-    data_regex = re.compile(u"""        #utf-8编码
-        年 |
-        月 |
-        日 |
-        (周一) |
-        (周二) | 
-        (周三) | 
-        (周四) | 
-        (周五) | 
-        (周六)
-    """, re.VERBOSE)
-    #剔除所有数字
-    decimal_regex = re.compile(r"[^a-zA-Z]\d+")
-    #剔除空格
-    space_regex = re.compile(r"\s+")
 
-    line = username_regex.sub(r"", line)
-    line = url_regex.sub(r"", line)
-    line = data_regex.sub(r"", line)
-    line = decimal_regex.sub(r"", line)
-    line = space_regex.sub(r"", line)
 
-    return line
 
-re_strs_list = regex_change(strs_list)
+
+# 测试数据
+test_data = ['很多城市的污水防治没有完全落实，存在很多的污水防治问题']
+test_cut = jieba.cut(test_data)
+test_corpus = [dictionary.doc2bow(test_data)]
+
+test_result = model[test_corpus]
+
+
+
+from gensim.test.utils import common_corpus, common_dictionary, get_tmpfile
+from gensim.models import LsiModel
+list(common_dictionary)
+model = LsiModel(common_corpus[:3], id2word=common_dictionary)  # train model
+vector = model[common_corpus[4]]  # apply model to BoW document
+model.add_documents(common_corpus[4:])  # update model with new documents
+tmp_fname = get_tmpfile("lsi.model")
+model.save(tmp_fname)  # save model
+loaded_model = LsiModel.load(tmp_fname)  # load model
+
+
+
+
+
+
+
