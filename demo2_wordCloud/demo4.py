dfd8bc4dd9b8f3cadada1b7ebe9258470ff30ad4
roczhang <1587839905@qq.com>
2022-01-09 21:46:45 +0800

    wordcloud

--- demo2_wordCloud/demo4.py
+++ demo2_wordCloud/demo4.py
@@ -1,13 +1,15 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt#绘图库
 import jieba
 from wordcloud import WordCloud
+import imageio
+# laptop上写的这个代码，由于win和mac不一样，所以它读文件需要使用utf-8
+# 这个代码是第二份文件的，与demo3相比只是文件的位置不同。
 
-# 这是第二次发的文件
 stopwords = [line.strip() for line in
-             open('/Volumes/roczhang/download/词频图/停用词表.txt',
+             open(r'C:\roczhang\WHPU\zen\词频图\停用词表.txt',
                   encoding='UTF-8').readlines()]
 # 读入文本数据
-fp = open(r'/Volumes/roczhang/WHPU/zen/merged.txt','r',encoding='utf-8')
+fp = open(r'C:\roczhang\WHPU\zen\2022 1 90\merged.txt','r',encoding='utf-8')
 content = fp.read()
 # print(content)
 #分词
@@ -34,24 +36,23 @@ for i in range(20):
     # print(hist[i])
     print('{:<10}{:>5}'.format(hist[i][0],hist[i][1]))#左对齐10，右对齐5个长度
 
-"第六" in stopwords
 
 result = ' '.join(data)
-# img = imageio.imread(r"/Volumes/roczhang/download/词频图/中国地图.jpg")
+img = imageio.imread(r"C:\roczhang\WHPU\zen\img\cycle.jpg")
 # print(result)
 #生成词云
 wc = WordCloud(
-    font_path=r'/Volumes/roczhang/font/思源宋体SC-Regular.otf',
+    font_path=r'C:\roczhang\font\font.otf',
     background_color = 'white',#背景颜色
     width=500,#图片的宽
     height=300,
     max_font_size=30,
-    min_font_size=7,
-    # mask=img,
+    min_font_size=5,
+    mask=img,
 )
 wc.generate(result)
 
-wc.to_file(r'/Volumes/roczhang/temp/wordcloud.png')#保存图片
+wc.to_file(r'C:\roczhang\WHPU\zen\img\wordcloud2.png')#保存图片
 #显示图片
 plt.figure('政府工作报告')
 plt.imshow(wc)
--- demo2_wordCloud/merge_files.py
+++ demo2_wordCloud/merge_files.py
@@ -1,10 +1,12 @@
 import os
-path = "/Volumes/roczhang/WHPU/zen/text2"
+# 将一个文件夹的文件合并
+
+path = r"C:\roczhang\WHPU\zen\2022 1 90"
 file_list = os.listdir(path)
-f = open('/Volumes/roczhang/WHPU/zen/merged.txt', 'w')
+f = open(r'C:/roczhang/WHPU/zen/2022 1 90/merged.txt', 'w', encoding='utf-8')
 for filename in file_list:
     filepath = path + '/' + filename
-    for line in open(filepath):
+    for line in open(filepath, encoding='utf-8'):
         f.writelines(line)
 f.close()
 
